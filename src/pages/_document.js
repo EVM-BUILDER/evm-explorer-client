c636a93a9d173922040eb7e2ea89ce0dddcc4e50
dilysngo <ngohung2393@gmail.com>
2023-02-23 11:22:40 +0700

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,72 +2,73 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class CustomDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-        })
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+                })
 
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
-    }
-  }
-  render() {
-    const settings = this.props.__NEXT_DATA__.props.globalProps?.settings
-    const rootScript = settings?.script
-
-    return (
-      <Html>
-        <Head>
-          <link rel="icon" href="/favicon.ico" type="image/png" sizes="16x16" />
-          <link
-            href={
-              settings?.ggfont || 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=optional'
+            const initialProps = await Document.getInitialProps(ctx)
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
             }
-            rel="stylesheet"
-            async
-          />
-          {rootScript?.header && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: rootScript.header,
-              }}
-            />
-          )}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          {rootScript?.body && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: rootScript.body,
-              }}
-            />
-          )}
-        </body>
-        {rootScript?.footer && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: rootScript.footer,
-            }}
-          />
-        )}
-      </Html>
-    )
-  }
+        } finally {
+            sheet.seal()
+        }
+    }
+    render() {
+        const settings = this.props.__NEXT_DATA__.props.globalProps?.settings
+        const rootScript = settings?.script
+
+        return (
+            <Html>
+                <Head>
+                    {settings && <link rel="icon" href="/favicon.ico" type="image/png" sizes="16x16" />}
+                    <link
+                        href={
+                            settings?.ggfont ||
+                            'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=optional'
+                        }
+                        rel="stylesheet"
+                        async
+                    />
+                    {rootScript?.header && (
+                        <script
+                            dangerouslySetInnerHTML={{
+                                __html: rootScript.header,
+                            }}
+                        />
+                    )}
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                    {rootScript?.body && (
+                        <script
+                            dangerouslySetInnerHTML={{
+                                __html: rootScript.body,
+                            }}
+                        />
+                    )}
+                </body>
+                {rootScript?.footer && (
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: rootScript.footer,
+                        }}
+                    />
+                )}
+            </Html>
+        )
+    }
 }
