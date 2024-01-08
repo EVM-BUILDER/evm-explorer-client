ad114d71478da5eb68b157fd2aa55259d9b9d980
thanhdev2k <thanhdev.crelife@gmail.com>
2024-01-08 22:17:57 +0700

    fix bug

--- src/components/Menu/config.js
+++ src/components/Menu/config.js
@@ -1,76 +1,76 @@
 const config = () => [
-  {
-    label: 'Home',
-    href: '/',
-    items: [],
-  },
-  {
-    label: 'Blockchain',
-    items: [
-      {
-        label: 'View Txns',
-        href: '/txs',
-        showBorder: true,
-      },
-      {
-        label: 'View Blocks',
-        href: '/blocks',
-        showBorder: true,
-      },
-      {
-        label: 'Top Accounts',
-        href: '/accounts',
-      },
-      {
-        label: 'Verified Contracts',
-        href: '/contractsVerified',
-      },
-    ],
-  },
-  {
-    label: 'Tokens',
-    items: [
-      {
-        label: 'PN20 Top Tokens',
-        href: '/tokens',
-      },
-      {
-        label: 'View PN20 Transfers',
-        href: '/tokens-transfers',
-        showBorder: true,
-      },
-      {
-        label: 'Top Statistics',
-        href: '/topstatistics',
-      },
-    ],
-  },
-  {
-    label: 'Resources',
-    items: [
-      {
-        label: 'Top Statistics',
-        href: '/topstat',
-      },
-      {
-        label: 'View Blocks',
-        href: '/blocks',
-      },
-    ],
-  },
-  {
-    label: 'More',
-    items: [
-      {
-        label: 'Api Documentation',
-        href: 'https://docs.com',
-      },
-      {
-        label: 'View Blocks',
-        href: '/blocks',
-      },
-    ],
-  },
+    {
+        label: 'Home',
+        href: '/',
+        items: [],
+    },
+    {
+        label: 'Blockchain',
+        items: [
+            {
+                label: 'View Txns',
+                href: '/txs',
+                showBorder: true,
+            },
+            {
+                label: 'View Blocks',
+                href: '/blocks',
+                showBorder: true,
+            },
+            {
+                label: 'Top Accounts',
+                href: '/accounts',
+            },
+            {
+                label: 'Verified Contracts',
+                href: '/contractsVerified',
+            },
+        ],
+    },
+    {
+        label: 'Tokens',
+        items: [
+            {
+                label: 'PN20 Top Tokens',
+                href: '/tokens',
+            },
+            {
+                label: 'View PN20 Transfers',
+                href: '/tokens-transfers',
+                showBorder: true,
+            },
+            // {
+            //   label: 'Top Statistics',
+            //   href: '/topstatistics',
+            // },
+        ],
+    },
+    {
+        label: 'Resources',
+        items: [
+            {
+                label: 'Top Statistics',
+                href: '/topstat',
+            },
+            {
+                label: 'View Blocks',
+                href: '/blocks',
+            },
+        ],
+    },
+    {
+        label: 'More',
+        items: [
+            {
+                label: 'Api Documentation',
+                href: 'https://docs.com',
+            },
+            {
+                label: 'View Blocks',
+                href: '/blocks',
+            },
+        ],
+    },
 ]
 
 export default config
