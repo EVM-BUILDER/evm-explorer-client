f42a7e0eb9033e13b1cd52f0652d34f700a00cc3
thanhdev21 <thanhdev.crelife@gmail.com>
2024-07-13 22:19:54 +0700

    fix bug

--- src/redux/token/hooks/useFetchTxsErc20.js
+++ src/redux/token/hooks/useFetchTxsErc20.js
@@ -9,6 +9,40 @@ function useFetchTxsErc20(address, page, page_size) {
     const [paramsTxsErc20, setParamsTxsErc20] = useState({
         page: page || 1,
         page_size: page_size || 50,
+        a: address,
+    })
+
+    const fetchTxsErc20 = useCallback(() => {
+        if (paramsTxsErc20.a) {
+            dispatch(getTxsErc20(paramsTxsErc20))
+        }
+    }, [dispatch, paramsTxsErc20])
+
+    useEffect(() => {
+        fetchTxsErc20()
+    }, [fetchTxsErc20])
+
+    useEffect(() => {
+        if (address) {
+            setParamsTxsErc20((prev) => ({ ...prev, a: address }))
+        }
+    }, [address])
+
+    return {
+        txsErc20: txsErc20[paramsTxsErc20.a],
+        paramsTxsErc20,
+        setParamsTxsErc20,
+        fetchTxsErc20,
+    }
+}
+
+export const useFetchTxsErc20WithCA = (address, page, page_size) => {
+    const dispatch = useDispatch()
+    const { txsErc20 } = useSelector((state) => state.Token)
+
+    const [paramsTxsErc20, setParamsTxsErc20] = useState({
+        page: page || 1,
+        page_size: page_size || 50,
         ca: address,
     })
 
