e26dca26e0197f461f379231ca89536cf1039e32
thanhdev21 <thanhdev.crelife@gmail.com>
2024-07-17 00:26:20 +0700

    fix bug

--- src/pages/admin/settings/systemsettings.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import AdminLayout from 'layouts/AdminLayout'
-import AdminSystemSettingsView from 'views/Admin/Settings/systemsettings'
-
-export default function AdminSystemSettings() {
-    return <AdminSystemSettingsView />
-}
-
-AdminSystemSettings.Layout = AdminLayout
