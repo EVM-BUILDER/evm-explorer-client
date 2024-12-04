543985e00c06d72693607b865446932e5bcff313
Nguyen Hop Thanh <85117731+thanhdev21@users.noreply.github.com>
2024-12-04 19:25:46 +0700

    update admin (#127)

--- src/views/Admin/Settings/chain.js
+++ src/views/Admin/Settings/chain.js
@@ -10,43 +10,43 @@ import Breadcrumb from 'components/Breadcrumb'
 const { Panel } = Collapse
 
 const breadcrumb = [
-  {
-    link: "/admin",
-    title: "Dashboard"
-  },
-  {
-    link: "/admin/settings/general",
-    title: "Settings",
-    isCurrent: true,
-  },
-  {
-    link: "/admin/settings/chain",
-    title: "Chain",
-    isCurrent: true,
-  }
+    {
+        link: '/admin',
+        title: 'Dashboard',
+    },
+    {
+        link: '/admin/settings/general',
+        title: 'Settings',
+        isCurrent: true,
+    },
+    {
+        link: '/admin/settings/chain',
+        title: 'Chain',
+        isCurrent: true,
+    },
 ]
 
 const SettingsChain = () => {
-  const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-  const { adminSettings } = useSelector((state) => state.Settings)
+    const { adminSettings } = useSelector((state) => state.Settings)
 
-  useEffect(() => {
-    dispatch(getAdminSettings())
-  }, [dispatch])
+    useEffect(() => {
+        dispatch(getAdminSettings())
+    }, [dispatch])
 
-  return (
-    <>
-      <Breadcrumb listItems={breadcrumb} />
-      <Space direction="vertical" className="admin-setting-wrapper">
-        <Collapse defaultActiveKey={['1']}>
-          <Panel header="Chain" key="1">
-            <ChainForm settings={adminSettings} data={adminSettings?.chain || {}} />
-          </Panel>
-        </Collapse>
-      </Space>
-    </>
-  )
+    return (
+        <>
+            <Breadcrumb listItems={breadcrumb} />
+            <Space direction="vertical" className="admin-setting-wrapper">
+                <Collapse defaultActiveKey={['1']}>
+                    <Panel header="Chain" key="1">
+                        <ChainForm settings={adminSettings} data={adminSettings?.chain || {}} />
+                    </Panel>
+                </Collapse>
+            </Space>
+        </>
+    )
 }
 
 SettingsChain.Layout = AdminLayout
