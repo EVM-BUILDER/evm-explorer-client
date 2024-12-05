c4bc72ea082895e211a86cacdc0b16e4354110b4
thanhdev21 <thanhdev.crelife@gmail.com>
2024-12-05 08:42:14 +0700

    update UI

--- src/views/Unitconverter/index.js
+++ src/views/Unitconverter/index.js
@@ -1,31 +1,33 @@
 import React from 'react'
 import PublicLayoutBlock from 'layouts/PublicLayoutBlock'
 import BlockUnitconverter from './components/BlockUnitconverter'
+import { useSettings } from 'redux/settings/hooks'
 
 const Unitconverter = () => {
-  return (
-    <div className="unitconverter-page">
-      <h1>Unit Converter</h1>
-      <div className="container">
-        <p>
-          PULSCAN or PULSE is often used in different denominations of its currency, such as Wei for interacting with
-          smart contracts and Gwei for calculating gas prices. Use our Unit Converter to easily convert between them!
-          For more info on unit conversion, read our <p>knowledge base article.</p>
-        </p>
-        <div className="block">
-          <a href="#">[Reset]</a>
+    const settings = useSettings()
+    return (
+        <div className="unitconverter-page">
+            <h1>Unit Converter</h1>
+            <div className="container">
+                <p>
+                    PULSCAN or PULSE is often used in different denominations of its currency, such as Wei for interacting with
+                    smart contracts and {settings?.chain?.unit || 'Gwei'} for calculating gas prices. Use our Unit Converter to
+                    easily convert between them! For more info on unit conversion, read our <p>knowledge base article.</p>
+                </p>
+                <div className="block">
+                    <a href="#">[Reset]</a>
 
-          <BlockUnitconverter />
-          <BlockUnitconverter />
-          <BlockUnitconverter />
-          <BlockUnitconverter />
-          <BlockUnitconverter />
-          <BlockUnitconverter />
-          <BlockUnitconverter />
+                    <BlockUnitconverter />
+                    <BlockUnitconverter />
+                    <BlockUnitconverter />
+                    <BlockUnitconverter />
+                    <BlockUnitconverter />
+                    <BlockUnitconverter />
+                    <BlockUnitconverter />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 Unitconverter.Layout = PublicLayoutBlock
